078de38142174f2d4fdfb6c284d0d486946d2c81
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-11-25 15:55:24 +0700

    Minor Change Route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,6 +20,43 @@ $routes->setAutoRoute(true);
 /**
  * @var RouteCollection $routes
  */
+
+
+// Halaman utama
+$routes->get('/', 'Home::index');
+
+// Routes untuk Login dan Registrasi
+$routes->get('login', 'LoginController::index');
+$routes->post('login', 'LoginController::authenticate');
+$routes->get('logout', 'LoginController::logout');
+$routes->get('registrasi', 'RegistrasiController::index');
+$routes->post('registrasi', 'RegistrasiController::store');
+
+// // Routes khusus untuk admin
+// $routes->group('admin', ['filter' => 'role:admin'], function ($routes) {
+//     $routes->get('dashboard', 'AdminController::dashboard');
+//     $routes->get('produk', 'ProdukController::index');
+//     $routes->get('produk/create', 'ProdukController::create');
+//     $routes->post('produk/store', 'ProdukController::store');
+//     $routes->get('produk/edit/(:num)', 'ProdukController::edit/$1');
+//     $routes->post('produk/update/(:num)', 'ProdukController::update/$1');
+//     $routes->get('sertifikat', 'SertifikatController::index');
+//     $routes->get('sertifikat/create', 'SertifikatController::create');
+//     $routes->post('sertifikat/store', 'SertifikatController::store');
+//     $routes->get('sertifikat/edit/(:num)', 'SertifikatController::edit/$1');
+//     $routes->post('sertifikat/update/(:num)', 'SertifikatController::update/$1');
+// });
+
+// // Routes khusus untuk user
+// $routes->group('user', ['filter' => 'role:user'], function ($routes) {
+//     $routes->get('dashboard', 'UserController::dashboard');
+//     $routes->get('pameran', 'PameranController::index');
+//     $routes->get('ekspor', 'EksporController::index');
+//     $routes->get('ekspor/create', 'EksporController::create');
+//     $routes->post('ekspor/store', 'EksporController::store');
+// });
+
+
 // Route Produk
 $routes->get('/produk', 'ProdukController::index');
 $routes->get('/produk/create', 'ProdukController::create');
