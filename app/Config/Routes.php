c7caac6ad2021b040ac264f2796458206d0c5f6a
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-11-25 15:13:20 +0700

    Route Media

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,3 +31,13 @@ $routes->post('/pameran/update/(:num)', 'PameranController::update/$1');
 $routes->get('/ekspor', 'EksporController::index');
 $routes->get('/ekspor/create', 'EksporController::create');
 $routes->post('ekspor/store', 'EksporController::store');
+$routes->get('/ekspor/edit/(:num)', 'EksporController::edit/$1');
+$routes->post('/ekspor/update/(:num)', 'EksporController::update/$1');
+
+// Route Media
+$routes->get('/media', 'MediaController::index');
+$routes->get('/media/create', 'MediaController::create');
+$routes->post('media/store', 'MediaController::store');
+$routes->get('/media/edit/(:num)', 'MediaController::edit/$1');
+$routes->post('/media/update/(:num)', 'MediaController::update/$1');
+
