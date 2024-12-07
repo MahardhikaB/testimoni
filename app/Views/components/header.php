acd7a5a3de74c53cfe1c7fca2fa99916fe424d1b
bryantosin1 <bryantosin1@gmail.com>
2024-12-07 12:01:07 +0700

    Header Logout

--- app/Views/components/header.php
+++ app/Views/components/header.php
@@ -10,12 +10,16 @@
     <div class="column-2">
         <a href="/user/dashboard">Beranda</a>
         <a href="/user/profile">Profile Perusahaan</a>
-        <a href="/progress">Progress Perusahaan</a>
+        <a href="/user/progress">Progress Perusahaan</a>
     </div>
 
     <!-- Sign In Button -->
-    <div class="column-3">
-        <a href="/registrasi">Sign In</a>
+    <div class="column-3 ">
+        <p>
+            <?= session()->get('nama_user') ?: 'User' ?>
+        </p>
+
+        <a href="<?= base_url('/logout') ?>" class="btn btn-default btn-flat float-end">Keluar</a> 
     </div>
 
     <!-- Menu Toggle for Mobile -->
@@ -23,18 +27,26 @@
 
     <!-- Dropdown Menu for Mobile -->
     <div class="menu-dropdown">
-        <a href="/dashboard">Beranda</a>
-        <a href="/profile">Profile Perusahaan</a>
-        <a href="/progress">Progress Perusahaan</a>
+        <a href="/user/dashboard">Beranda</a>
+        <a href="/user/profile">Profile Perusahaan</a>
+        <a href="/user/progress">Progress Perusahaan</a>
         <a href="/registrasi">Sign In</a>
     </div>
-</nav>
 
-<script>
+    <script>
     function toggleMenu() {
         const toggle = document.querySelector('.menu-toggle');
         const dropdown = document.querySelector('.menu-dropdown');
         toggle.classList.toggle('active');
         dropdown.classList.toggle('active');
     }
+
+    document.querySelector('.user-image').addEventListener('click', function() {
+    const dropdownMenu = document.querySelector('.user-menu .dropdown-menu');
+    dropdownMenu.classList.toggle('show');
+});
+
 </script>
+</nav>
+
+
