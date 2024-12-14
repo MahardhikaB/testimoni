70ae6da8ce49b85c79c26a721770313a4d8502dc
bryantosin1 <bryantosin1@gmail.com>
2024-12-14 12:30:26 +0700

    fix 2 migration hehe

--- app/Database/Migrations/2024-11-16-033700_CreateUsers.php
+++ app/Database/Migrations/2024-11-16-033700_CreateUsers.php
@@ -40,11 +40,6 @@ class CreateUsers extends Migration
                 'type'    => 'DATETIME',
                 'null'    => true,
             ],
-            'status_verifikasi' => [
-                'type'       => 'ENUM',
-                'constraint' => ['pending', 'accepted','rejected' ],
-                'default'    => 'pending',
-            ],
         ]);
 
         $this->forge->addKey('user_id', true);
